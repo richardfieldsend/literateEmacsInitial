e5e5103a89a65842b89d3b555d156202ca1e8a2f
Richard Fieldsend <rf343@cam.ac.uk>
2020-04-27 23:58:54 +0100

    Add Word Count function displayed on the modeline.

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode text-mode))
+ '(doom-modeline-enable-word-count t)
  '(package-selected-packages '(use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
